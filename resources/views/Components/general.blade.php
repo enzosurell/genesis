c94f18bf98d6a4f033a37172faaccf01d3342e4c
enzo <renzosurell@gmail.com>
2024-07-26 01:50:02 +0800

    c2

--- resources/views/Components/general.blade.php
+++ resources/views/Components/general.blade.php
@@ -8,9 +8,9 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    
+
 </head>
-<body class="">
+<body data-bs-theme="dark">
     
 </body>
 </html>
